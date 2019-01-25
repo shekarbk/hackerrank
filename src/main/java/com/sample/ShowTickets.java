4760258785fc55d0d7f37098f8d79cb1b71f5bfd
chandra <shekar.bk@gmail.com>
2019-01-26 05:19:12 +0530

    added junit test cases

--- src/main/java/com/sample/ShowTickets.java
+++ src/main/java/com/sample/ShowTickets.java
@@ -6,26 +6,6 @@ import java.util.*;
  * This code will find show tickets timing
  */
 public class ShowTickets {
-    public static void main(String[] args) {
-        //Test case 1
-        List<Integer> tickets = Arrays.asList(2, 6, 3, 4, 5);
-        int position = 1;
-        int timeCounter = calculateTimer(tickets, position);
-        System.out.println("For position " + position + " time taken: " + timeCounter);
-
-        //Test case 2
-        tickets = Arrays.asList(13);
-        position = 0;
-        timeCounter = calculateTimer(tickets, position);
-        System.out.println("For position " + position + " time taken: " + timeCounter);
-
-        //Test case 3
-        tickets = Arrays.asList(1, 2, 5);
-        position = 1;
-        timeCounter = calculateTimer(tickets, position);
-        System.out.println("For position " + position + " time taken: " + timeCounter);
-
-    }
 
     /**
      * Calculate time taken for each ticket
@@ -36,7 +16,7 @@ public class ShowTickets {
     static int calculateTimer(List<Integer> tickets, int position) {
         int queueLength = tickets.size() - 1;
         int index = 0;
-        int timeCounter = 1;
+        int timeCounter = 0;
 
         List<Integer> ticketPosition = setPositionIndicator(queueLength, position);
 
@@ -47,7 +27,7 @@ public class ShowTickets {
             } else {
                 int personTicket = tickets.get(index);
                 personTicket--;
-                if (personTicket > 0) {
+                if (personTicket >= 0) {
                     timeCounter++;
                 }
                 tickets.set(index, personTicket);
---
+++ src/test/java/com/sample/ShowTicketsTest.java
@@ -0,0 +1,38 @@
+package com.sample;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShowTicketsTest {
+
+    @Test
+    public void test5TicketQueuePosition2() {
+        ShowTickets ticketObj = new ShowTickets();
+        List<Integer> tickets = Arrays.asList(2, 6, 3, 4, 5);
+        int position = 2;
+        int timeCounter = ticketObj.calculateTimer(tickets, position);
+        assertEquals(12, timeCounter);
+    }
+
+    @Test
+    public void test1TicketQueuePosition0() {
+        ShowTickets ticketObj = new ShowTickets();
+        List<Integer> tickets = Arrays.asList(13);
+        int position = 0;
+        int timeCounter = ticketObj.calculateTimer(tickets, position);
+        assertEquals(13, timeCounter);
+    }
+
+    @Test
+    public void test3TicketQueuePosition1() {
+        ShowTickets ticketObj = new ShowTickets();
+        List<Integer> tickets = Arrays.asList(1, 2, 5);
+        int position = 1;
+        int timeCounter = ticketObj.calculateTimer(tickets, position);
+        assertEquals(4, timeCounter);
+    }
+}
